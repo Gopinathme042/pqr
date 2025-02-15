613455ecd71dc97eae8e5721b4d51489a96456c5
Gopinath7829 <gopinathme042@gmail.com>
2025-02-15 19:37:19 +0530

    commit two

--- src/main/java/com/book/packageNew/ClassNew.java
+++ src/main/java/com/book/packageNew/ClassNew.java
@@ -2,13 +2,15 @@ package com.book.packageNew;
 
 public class ClassNew {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		int num=8;
 		if(num%2==0) {
 			System.out.println(num+" is Even number");
+			Thread.sleep(3000);
 		}
 		else {
 			System.out.println(num+" is Odd number");
+			Thread.sleep(3000);
 		}
 
 	}
